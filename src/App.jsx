64232b8f99e101ce94ffc8684970e4454a696c9b
BabeKung02 <NTPBabe@gmail.com>
2023-01-14 20:04:06 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import {useState , useEffect} from "react";
+import React, {useState , useEffect} from "react";
 import MovieList from "./components/MovieList";
 import './App.css';
+import Cart from "./components/Cart";
 
+let Logo = ["BabeMovie"];
 let API_key="&api_key=88c1d19be3079c6ade78443d397567f5";
 let base_url = "https://api.themoviedb.org/3";
 let url = base_url+"/discover/movie?sort_by=popularity.desc"+API_key;
-let arr=["BabeMovie"];
 
 const App = () => {
-    const [movieData,setData]=useState([]);
-    const [url_set,setUrl]=useState(url);
-    const [search,setSearch]=useState();
+    const [movieData,setData] = useState([]);
+    const [url_set,setUrl] = useState(url);
+    const [search,setSearch] = useState();
+    const [show,setShow] = useState(true);
 
     useEffect(()=>{
         fetch(url_set).then(res=>res.json()).then(data=>{
@@ -28,56 +30,51 @@ const App = () => {
         {
             url = base_url+"/search/movie?api_key=88c1d19be3079c6ade78443d397567f5&query="+search;
             setUrl(url);
-            setSearch(" ");
+            setSearch("");
         }
     }
 
     return(
         <>
-            <div className="header">
-                <nav>
-                    <ul>
-                        {
-                            arr.map((value,index)=>{ 
-                                return(
-                                    <li><a href="#" key={index} name={value} onClick={(e)=>{getData(e.target.name)}}>{value} </a></li>
-                                )
-                            })
-                        }
-                       
-                    </ul>
-                </nav>
-                
-                    <div className="search-btn">
+          <div className="header">
+            <nav>
+              <ul>
+                  {Logo.map((value,index)=>{ 
+                    return(
+                        <li><a href="#" key={index} name={value} onClick = {(e)=>{getData(e.target.name)}}>{value} </a></li>
+                    )   
+                })
+            }
+        </ul>    
+    </nav>
+          <div className="search-btn">
     
-                          <input type="text" placeholder="Enter Movie Name" 
-                          className="inputText" onChange={(e)=>{setSearch(e.target.value)}} 
-                          value={search} onKeyPress={searchMovie}>
-                          </input>
-
-                            <button><i className="fas fa-search"></i></button>
-                            
-                        <div className="cart">
-                         <span className="span1">
-                        <i className="fas fa-cart-plus" ></i>
-                        <span className="span2">0</span>
-                      </span>
-                    
-                    </div>
-                </div>  
-            </div>
-
+           <input type="text" placeholder="Enter Movie Name" 
+            className="inputText" onChange={(e)=>{setSearch(e.target.value)}} 
+            value={search} onKeyPress={searchMovie}>
+            </input>
+        
+              <button><i className="fas fa-search"></i></button>
+                
+                <div className="cart" onClick={(e) => setShow(false) } >
+                  <span className="span1">
+                    <i className="fas fa-cart-plus" ></i>
+                  </span>
+                      
+                </div>
+            </div>  
+    </div>
+    
             <div className="container">
                 {
-                    (movieData.length==0)?<p className="notfound">Not Found</p>: movieData.map((res,pos)=>{
+                    
+                    (movieData.length==0)?<p className="notfound">Not Found</p>: movieData.map((item,index)=>{
                         return(
-                            <MovieList info={res} key={pos}/>
+                            show ? <MovieList info={item} key={index}/> : <Cart/>
+                        
                         )
                     })
                 }
-            </div>
-            <div> 
-             
             </div>
         </>
     )
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,18 +1,23 @@
 import react from "react";
 
+let img_path="https://image.tmdb.org/t/p/w500";
+let price = '200 Bath'
+
 const MovieList=(movie)=>{
-
-    let img_path="https://image.tmdb.org/t/p/w500";
-    let price = '200 Bath'
-
+    
+    function handleClick(event){
+        alert(`คุณได้เพิ่มสินค้า ${movie.info.title} เข้าไปยังรถเข็น`)
+    }
     return(
+        
         <div>
             <div className="movie">
                 <img src={img_path + movie.info.poster_path} className="poster"></img>
                 <div className="movie-details">
                     <div className="box">
                         <h4 className="title">{movie.info.title}</h4>
-                        <button className="price">{price}</button>
+                        <button className="price" onClick={handleClick}>{price}</button>
+                        
                     </div>
                 </div>
             </div>
